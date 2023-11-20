04759db167077fa1ab047ddc0e78cbbfcb5ae772
DmitryLysikov <129677338+DmitryLysikov@users.noreply.github.com>
2023-11-20 13:16:46 +0500

    Add files via upload

---
+++ untitled35/src/Excel.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Excel {
+    public static void main(String[] args) throws Exception {
+        try {
+
+            FileInputStream excelFile = new FileInputStream("C:\\Users\\lysik\\Downloads\\JAVA\\basicprogramming.xlsx");
+            XSSFWorkbook workbook = new XSSFWorkbook(excelFile);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext()){
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()){
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+                        case NUMERIC -> System.out.print(cell.getNumericCellValue() + "\t");
+                        case STRING -> System.out.print(cell.getStringCellValue() + "\t\t");
+                    }
+                }
+                System.out.println();
+            }
+            excelFile.close();
+        }catch (Exception e){
+            System.out.println("WHAT?????");
+        }
+    }
+}
+
